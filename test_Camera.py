12b9edf448d67d078b0c42fe6d45a25e4f357847
zoy283 <zoy283@qq.com>
2022-06-29 20:43:23 +0800

    camera&checkcolor

--- test_Camera.py
+++ test_Camera.py
@@ -4,7 +4,8 @@ import sys
 
 arg = sys.argv
 # print(arg)
-cap=cv2.VideoCapture(int(arg[1]))
+# cap=cv2.VideoCapture(int(arg[1]))
+cap=cv2.VideoCapture(0)
 cap.set(3,320)
 cap.set(4,240)
 #cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
@@ -12,7 +13,7 @@ while (True):
     ret,frame=cap.read()
     if ret == True:
         cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xff == ord('q'):
+        if cv2.waitKey(1) & 0xff == 27: # ESC
             break
     else:
         break
